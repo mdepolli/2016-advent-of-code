12e1103500cb5e489fecf91ead695f6551c517bb
Marcelo De Polli <marcelo.polli@gmail.com>
2018-11-16 12:30:11 -0200

    Mix format

--- day_1a/lib/day_1a.ex
+++ day_1a/lib/day_1a.ex
@@ -1,16 +1,16 @@
 defmodule Day1a do
   def distance_in_blocks(str) do
     str
-      |> treat_data
-      |> count_lengths
-      |> calculate_distance
+    |> treat_data
+    |> count_lengths
+    |> calculate_distance
   end
 
   defp treat_data(str) do
     str
-      |> String.strip
-      |> String.split(", ")
-      |> convert_pairs
+    |> String.strip()
+    |> String.split(", ")
+    |> convert_pairs
   end
 
   defp convert_pairs(old_list, new_list \\ [])
@@ -20,32 +20,45 @@ defmodule Day1a do
       case hand_direction do
         "L" ->
           "W"
+
         "R" ->
           "E"
       end
+
     convert_pairs(tail, [{cardinal_direction, String.to_integer(length)}])
   end
 
-  defp convert_pairs([<<hand_direction::bytes-size(1)>> <> length | tail], converted_list = [{previous_direction, _} | _]) do
+  defp convert_pairs(
+         [<<hand_direction::bytes-size(1)>> <> length | tail],
+         converted_list = [{previous_direction, _} | _]
+       ) do
     cardinal_direction =
       case {hand_direction, previous_direction} do
         {"L", "N"} ->
           "W"
+
         {"L", "W"} ->
           "S"
+
         {"L", "S"} ->
           "E"
+
         {"L", "E"} ->
           "N"
+
         {"R", "N"} ->
           "E"
+
         {"R", "W"} ->
           "N"
+
         {"R", "S"} ->
           "W"
+
         {"R", "E"} ->
           "S"
       end
+
     convert_pairs(tail, [{cardinal_direction, String.to_integer(length)} | converted_list])
   end
 
@@ -60,13 +73,17 @@ defmodule Day1a do
       case direction do
         "N" ->
           {x + length, y}
+
         "E" ->
           {x, y + length}
+
         "S" ->
           {x - length, y}
+
         "W" ->
           {x, y - length}
       end
+
     count_lengths(tail, {new_x, new_y})
   end
 
--- day_1b/lib/day_1b.ex
+++ day_1b/lib/day_1b.ex
@@ -1,15 +1,15 @@
 defmodule Day1b do
   def distance_of_first_double_visit(str) do
     str
-      |> treat_data
-      |> traverse_list
+    |> treat_data
+    |> traverse_list
   end
 
   defp treat_data(str) do
     str
-      |> String.strip
-      |> String.split(", ")
-      |> convert_pairs
+    |> String.strip()
+    |> String.split(", ")
+    |> convert_pairs
   end
 
   defp convert_pairs(old_list, new_list \\ [])
@@ -19,33 +19,51 @@ defmodule Day1b do
       case hand_direction do
         "L" ->
           "W"
+
         "R" ->
           "E"
       end
+
     convert_pairs(tail, List.duplicate(cardinal_direction, String.to_integer(length)))
   end
 
-  defp convert_pairs([<<hand_direction::bytes-size(1)>> <> length | tail], converted_list = [previous_direction | _]) do
+  defp convert_pairs(
+         [<<hand_direction::bytes-size(1)>> <> length | tail],
+         converted_list = [previous_direction | _]
+       ) do
     cardinal_direction =
       case {hand_direction, previous_direction} do
         {"L", "N"} ->
           "W"
+
         {"L", "W"} ->
           "S"
+
         {"L", "S"} ->
           "E"
+
         {"L", "E"} ->
           "N"
+
         {"R", "N"} ->
           "E"
+
         {"R", "W"} ->
           "N"
+
         {"R", "S"} ->
           "W"
+
         {"R", "E"} ->
           "S"
       end
-    convert_pairs(tail, List.flatten([List.duplicate(cardinal_direction, String.to_integer(length)) | converted_list]))
+
+    convert_pairs(
+      tail,
+      List.flatten([
+        List.duplicate(cardinal_direction, String.to_integer(length)) | converted_list
+      ])
+    )
   end
 
   defp convert_pairs([], converted_list) do
@@ -59,17 +77,21 @@ defmodule Day1b do
       case next_direction do
         "N" ->
           {x + 1, y}
+
         "E" ->
           {x, y + 1}
+
         "S" ->
           {x - 1, y}
+
         "W" ->
           {x, y - 1}
       end
+
     if {current_x, current_y} in visited_list do
       abs(current_x) + abs(current_y)
     else
-      traverse_list(tail, [{current_x, current_y} | visited_list] |> List.flatten)
+      traverse_list(tail, [{current_x, current_y} | visited_list] |> List.flatten())
     end
   end
 
